3659836b46edb92925d092e4c69deed1d8383bde
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-09-17 19:04:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,14 @@ function happyHolidays() {
   return "Happy Holidays!"
 }
 
-
+function happyHolidaysTo() {
+  return `Happy holidays, ${name}!`
+}
   
+function happyHolidayTo() {
+  return `Happy ${holiday}, ${name}!`
+  
+  function holidayCountdown() {
+    return `It's ${days} until ${holiday}!`
+  }
+}
